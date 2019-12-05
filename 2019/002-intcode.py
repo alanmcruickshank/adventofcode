265d40851692ec1831af6144d4c58484d2d26b47
Alan Cruickshank <alanmcruickshank@gmail.com>
2019-12-05 12:18:59 +0000

    Part 2 (puzzle 2)

--- 2019/002-intcode.py
+++ 2019/002-intcode.py
@@ -6,6 +6,12 @@ puzzle_code = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,5,19,23,2,9,23,27,1,27
 # Make sure we make the specified adjustments
 puzzle_code = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,5,19,23,2,9,23,27,1,27,5,31,2,31,13,35,1,35,9,39,1,39,10,43,2,43,9,47,1,47,5,51,2,13,51,55,1,9,55,59,1,5,59,63,2,6,63,67,1,5,67,71,1,6,71,75,2,9,75,79,1,79,13,83,1,83,13,87,1,87,5,91,1,6,91,95,2,95,13,99,2,13,99,103,1,5,103,107,1,107,10,111,1,111,13,115,1,10,115,119,1,9,119,123,2,6,123,127,1,5,127,131,2,6,131,135,1,135,2,139,1,139,9,0,99,2,14,0,0]
 
+def make_code(buff, noun, verb):
+    rbuff = buff.copy()
+    rbuff[1] = noun
+    rbuff[2] = verb
+    return rbuff
+
 
 example_code = [1,1,1,4,99,5,6,0,99]
 
@@ -33,10 +39,20 @@ def iter_codes(buff):
     idx = 0
     stop = False
     while True:
-        print((idx, buff))
+        # print((idx, buff))
         if stop:
             return buff
         buff, idx, stop = take_step(buff, idx=idx)
 
 
-iter_codes(puzzle_code)
+r = iter_codes(make_code(puzzle_code, 12, 2))
+print(r[0])
+
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        r = iter_codes(make_code(puzzle_code, noun, verb))
+        print(r[0], noun, verb)
+        if r[0] == 19690720:
+            break
+    if r[0] == 19690720:
+        break
