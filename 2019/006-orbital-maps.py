2868125923ce6ff0d004a20a7ff9338defb3c69d
Alan Cruickshank <alanmcruickshank@gmail.com>
2019-12-06 19:46:12 +0000

    day 6 part 2

--- 2019/006-orbital-maps.py
+++ 2019/006-orbital-maps.py
@@ -40,3 +40,42 @@ c = count_orbits(m)
 s = sum([c[k] for k in c])
 print(s)
 
+### Part 2 - orbital transfers
+
+# to get to santa, we need to work out the common parent, then go up to that and then down to santa.
+
+def path_to_com(m, e):
+    """returns a list of stops on the way from e to com."""
+    buff = []
+    i = e
+    while True:
+        if i == 'COM':
+            break
+        else:
+            i = m[i]
+            buff.append(i)
+    return buff
+
+def common_parent_hops(m, e1, e2):
+    p1 = path_to_com(m, e1)
+    p2 = path_to_com(m, e2)
+    p1.reverse()
+    p2.reverse()
+    i = 0
+    common = None
+    while True:
+        if p1[i] == p2[i]:
+            i += 1
+        else:
+            common = p1[i - 1]
+            break
+    # common is the common element.
+    d1 = len(p1) - i
+    d2 = len(p2) - i
+    return common, d1, d2, d1 + d2
+
+
+# print(path_to_com(m, 'SAN'))
+# print(path_to_com(m, 'YOU'))
+
+print(common_parent_hops(m, 'SAN', 'YOU'))
