a64e6a4dfe1038838fe06e0b2f2ada37b68e044e
Alan Cruickshank <alanmcruickshank@gmail.com>
2019-12-10 17:24:22 +0000

    Day 10 part 2

--- 2019/010-asteroid-maps.py
+++ 2019/010-asteroid-maps.py
@@ -50,31 +50,71 @@ class AsteroidField(object):
     @classmethod
     def from_file(cls, fname):
         buff = []
+        print("Operating on {0}".format(fname))
         with open(fname, 'r') as f:
             for line in f:
                 buff.append(line.strip('\n'))
         return cls(map_list=buff)
-    
+
+    def get_visible(self, roid):
+        buffer = {}
+        for roid_b in self.roid_list:
+            b, d = roid.bearing_to(roid_b)
+            if d == 0:
+                pass
+            elif b not in buffer or d < buffer[b][0]:
+                buffer[b] = (d, roid_b)
+            else:
+                # This isn't closer
+                pass
+        return buffer
+
     def assess(self):
         best = None
         for roid_a in self.roid_list:
-            buffer = {}
-            for roid_b in self.roid_list:
-                b, d = roid_a.bearing_to(roid_b)
-                if d == 0:
-                    pass
-                elif b not in buffer or d < buffer[b][0]:
-                    buffer[b] = (d, roid_b)
-                else:
-                    # This isn't closer
-                    pass
+            buffer = self.get_visible(roid_a)
             # print(roid_a, len(buffer.keys()))
             visible = len(buffer.keys())
             if best is None or visible > best[0]:
                 best = (visible, roid_a)
         return best
 
+    def iter_laser(self, roid=None, stop_at=10):
+        # initialise the laser pointing vertically.
+        # Bearing is defined zero at dead horizontal right and rotates
+        # clockwise to +pi. if at any point there are not points
+        # at a higher bearing, then we minus 2pi from it and carry on.
+        # ACTUALLY - do it in fractions so we're not messed up by floating point error
+        laser_bearing = (0, -1)  # -math.pi / 2
+        target_n = 1
+
+        # If roid is none, we first select the best asteroid
+        if roid is None:
+            _, roid = self.assess()
+
+        while True:
+            # Get the visible asteroids
+            visible = self.get_visible(roid)
+            # Make a list and sort by bearing FROM THE LASER
+            visible = [(b, math.atan2(b[1], b[0]) - math.atan2(laser_bearing[1], laser_bearing[0]), *visible[b]) for b in visible]
+            # For any bearings less than zero add 2pi
+            visible = [(b, a + (2 * math.pi) if a < 0 else a, d, r) for b, a, d, r in visible]
+            # Sort by angle from laser
+            visible = sorted(visible, key=lambda v: v[1])
+            # Identify the current target
+            target = visible[0][3]
+            print("Target #{0} is {1}".format(target_n, target))
+            target_n += 1
+            # Remove the target
+            self.roid_list.remove(target)
+            # Update the bearing to that of the NEXT ASTEROID BEFORE REMOVAL
+            laser_bearing = visible[1][0]
+            # for v in visible:
+            #    print(v)
+            if target_n > stop_at:
+                break
+
 
 m = AsteroidField.from_file('010-map-t.txt')
 
-print(m.assess())
+m.iter_laser(stop_at=200)
