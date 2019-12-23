90557ba5d19f1b8aaad3c126645da6bedbe1b31d
Alan Cruickshank <alanmcruickshank@gmail.com>
2019-12-23 21:55:40 +0000

    Day 11 part 1

--- 2019/011-intcode-paint.py
+++ 2019/011-intcode-paint.py
@@ -5,7 +5,77 @@ Not working. Can't get the right answer?
 
 
 class IntComp(object):
+
+    inbuilt_programs = {
+        'test_a': [
+            109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100,
+            16, 101, 1006, 101, 0, 99
+        ],
+        'asteroid': [
+            3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1101, 48, 82,
+            225, 102, 59, 84, 224, 1001, 224, -944, 224, 4, 224, 102, 8,
+            223, 223, 101, 6, 224, 224, 1, 223, 224, 223, 1101, 92, 58, 224,
+            101, -150, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224,
+            1, 224, 223, 223, 1102, 10, 89, 224, 101, -890, 224, 224, 4, 224,
+            1002, 223, 8, 223, 1001, 224, 5, 224, 1, 224, 223, 223, 1101, 29,
+            16, 225, 101, 23, 110, 224, 1001, 224, -95, 224, 4, 224, 102, 8,
+            223, 223, 1001, 224, 3, 224, 1, 223, 224, 223, 1102, 75, 72, 225,
+            1102, 51, 8, 225, 1102, 26, 16, 225, 1102, 8, 49, 225, 1001, 122,
+            64, 224, 1001, 224, -113, 224, 4, 224, 102, 8, 223, 223, 1001, 224,
+            3, 224, 1, 224, 223, 223, 1102, 55, 72, 225, 1002, 174, 28, 224,
+            101, -896, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 4, 224, 224,
+            1, 224, 223, 223, 1102, 57, 32, 225, 2, 113, 117, 224, 101, -1326,
+            224, 224, 4, 224, 102, 8, 223, 223, 101, 5, 224, 224, 1,
+            223, 224, 223, 1, 148, 13, 224, 101, -120, 224, 224, 4, 224, 1002,
+            223, 8, 223, 101, 7, 224, 224, 1, 223, 224, 223, 4, 223, 99, 0, 0,
+            0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227,
+            247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1,
+            99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0,
+            99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1,
+            99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1,
+            99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101,
+            314, 0, 0, 106, 0, 0, 1105, 1, 99999, 8, 677, 226, 224, 102, 2,
+            223, 223, 1006, 224, 329, 101, 1, 223, 223, 107, 677, 677, 224,
+            1002, 223, 2, 223, 1006, 224, 344, 101, 1, 223, 223, 8, 226, 677,
+            224, 102, 2, 223, 223, 1006, 224, 359, 101, 1, 223, 223, 107, 226,
+            226, 224, 102, 2, 223, 223, 1005, 224, 374, 1001, 223, 1, 223,
+            1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 389, 101, 1,
+            223, 223, 107, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 404,
+            1001, 223, 1, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1006,
+            224, 419, 1001, 223, 1, 223, 108, 677, 677, 224, 102, 2, 223, 223,
+            1005, 224, 434, 1001, 223, 1, 223, 1008, 677, 226, 224, 1002, 223,
+            2, 223, 1006, 224, 449, 1001, 223, 1, 223, 7, 226, 677, 224, 1002,
+            223, 2, 223, 1006, 224, 464, 1001, 223, 1, 223, 1007, 677, 677,
+            224, 102, 2, 223, 223, 1005, 224, 479, 1001, 223, 1, 223, 1007,
+            226, 226, 224, 1002, 223, 2, 223, 1005, 224, 494, 1001, 223, 1,
+            223, 108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 509, 1001,
+            223, 1, 223, 1007, 226, 677, 224, 1002, 223, 2, 223, 1006, 224,
+            524, 101, 1, 223, 223, 1107, 677, 677, 224, 102, 2, 223, 223,
+            1005, 224, 539, 101, 1, 223, 223, 1107, 677, 226, 224, 102, 2,
+            223, 223, 1005, 224, 554, 1001, 223, 1, 223, 108, 677, 226,
+            224, 1002, 223, 2, 223, 1006, 224, 569, 1001, 223, 1, 223, 1108,
+            226, 677, 224, 1002, 223, 2, 223, 1006, 224,
+            584, 101, 1, 223, 223, 8, 677, 677, 224, 1002, 223, 2, 223, 1006,
+            224, 599, 1001, 223, 1, 223, 1008, 226, 226, 224, 102, 2, 223,
+            223, 1006, 224, 614, 101, 1, 223, 223, 7, 677, 677, 224, 1002,
+            223, 2, 223, 1006, 224, 629, 101, 1, 223, 223, 1008, 677, 677,
+            224, 102, 2, 223, 223, 1005, 224, 644, 101, 1, 223, 223, 7, 677,
+            226, 224, 1002, 223, 2, 223, 1005, 224, 659, 101, 1, 223, 223,
+            1108, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 674, 1001,
+            223, 1, 223, 4, 223, 99, 226
+        ]
+    }
+
     def __init__(self, code):
+        # If code is a string, it might be a reference to an inbuilt program
+        if isinstance(code, str):
+            if code in self.inbuilt_programs:
+                code = self.inbuilt_programs[code]
+            else:
+                raise KeyError(
+                    "Provided unknown program reference: {0!r}".format(
+                        code
+                    ))
         # Make the memory a dictionary, so we can assign arbitrary values
         self.memory = {idx: val for idx, val in enumerate(code)}
         self.halt = False
@@ -33,7 +103,8 @@ class IntComp(object):
         p2_mode = (op_code % 10000) // 1000
         p3_mode = (op_code % 100000) // 10000
         output = None
-        # print(op_code, instruction, self.prog_pointer, input_buffer)
+        # print("...", self.prog_pointer, op_code, instruction, p1_mode,
+        #       p2_mode, p3_mode, input_buffer, self.rel_base)
 
         if instruction == 99:
             self.await_input = False
@@ -104,7 +175,7 @@ class IntComp(object):
         self.await_input = False
         return None, output
 
-    def run(self, input_buffer=None):
+    def run(self, input_buffer=None, interactive=False):
         if self.halt:
             raise RuntimeError("IntComp is in a halt state already!")
         elif self.await_input and input_buffer is None:
@@ -112,12 +183,25 @@ class IntComp(object):
         else:
             while True:
                 input_buffer, output = self.take_step(input_buffer=input_buffer)
-                if output:
-                    return output
+                if output is not None:
+                    if interactive:
+                        print("OUT:   {}".format(output))
+                    else:
+                        return output
                 if self.halt:
+                    if interactive:
+                        print("HALT")
                     break
                 if self.await_input:
-                    return
+                    if interactive:
+                        input_buffer = input("INPUT: ")
+                        if input_buffer != '':
+                            self.await_input = False
+                        else:
+                            print("FAILED INPUT")
+                            return
+                    else:
+                        return
 
 
 class HullGrid(object):
@@ -203,7 +287,7 @@ class HullPainterSim(object):
                     next_out_paint = True
             if self.comp.halt:
                 print("> Halt!: Ever painted: {0}".format(self.hull.num_painted()))
-                print("> Halt!: Ever painted: {0!r}".format(self.hull._ever_painted))
+                # print("> Halt!: Ever painted: {0!r}".format(self.hull._ever_painted))
                 break
             if self.comp.await_input:
                 # print("> Await Input! Loc is {0!r}, Dir is {1}".format(self.pos, self.dir))
@@ -214,5 +298,13 @@ class HullPainterSim(object):
                 # print("Input: {0}".format(input_buffer))
 
 
-r = HullPainterSim()
-r.run()
+def test():
+    # Test
+    c = IntComp('asteroid')
+    c.run(interactive=True)
+
+def challenge():
+    r = HullPainterSim()
+    r.run()
+
+challenge()
