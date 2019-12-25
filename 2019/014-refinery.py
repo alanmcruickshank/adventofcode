7d934dd68aa0b6f3b8adf743563dcac077f899f1
Alan Cruickshank <alanmcruickshank@gmail.com>
2019-12-25 22:13:51 +0000

    Ready to solve day 14 part 1?

--- 2019/014-refinery.py
+++ 2019/014-refinery.py
@@ -2,10 +2,34 @@
 
 
 class Recipe(object):
-    def __init__(self, recipe_buff):
+    def __init__(self, recipe_buff, raw='ORE'):
         self.recipe_buff = recipe_buff
-        self.goal = 'FUEL'
-        self.raw = 'ORE'
+
+        self.ingredient_possibilities = {}
+        for k in self.recipe_buff:
+            recipe_requires = list(self.recipe_buff[k][1].keys())
+            for ingredient in recipe_requires:
+                if ingredient not in self.ingredient_possibilities:
+                    self.ingredient_possibilities[ingredient] = []
+                self.ingredient_possibilities[ingredient].append(k)
+
+        self.element_levels = {raw: 1}
+        # Starting at ORE, work out levels as we go UP.
+        # We want the LONGEST path from each to ORE, rather
+        # than necessarily the shortest.
+        while True:
+            # Find an element in the next iteration
+            remaining = (set(self.ingredient_possibilities.keys())
+                         - set(self.element_levels.keys()))
+            if len(remaining) == 0:
+                break
+            for k in remaining:
+                # Can it be completely made?
+                requires = set(self.recipe_buff[k][1].keys())
+                if requires <= set(self.element_levels.keys()):
+                    max_len = max([self.element_levels[i] for i in requires])
+                    self.element_levels[k] = max_len + 1
+                    break
 
     @classmethod
     def from_file(cls, filename):
@@ -26,11 +50,90 @@ class Recipe(object):
             r_in = [elem.strip().split(' ') for elem in r_in]
             r_in = {name: int(val) for val, name in r_in}
             if r_out[0] in recipe_buff:
-                raise ValueError("Double setting a recipe for {0}!".format(r_out[0]))
+                raise ValueError("Double setting a recipe for {0}!".format(
+                    r_out[0]))
             recipe_buff[r_out[0]] = (r_out[1], r_in)
         # Instantiate the class
         return cls(recipe_buff)
 
-r = Recipe.from_file('014-recipe-basic.txt')
-print(r.recipe_buff)
+    def solve(self, goal='FUEL', raw='ORE'):
+        goal_units = self.recipe_buff[goal][0]
+        if goal_units != 1:
+            raise ValueError(
+                "End result {0} is made in units of {1}.".format(
+                    goal, goal_units))
+        buff = self.recipe_buff[goal][1]
+        # Print out result
+        # # print(buff)
 
+        # Iteratively solve
+        while True:
+            # Are we done?
+            if len(buff) == 1:
+                break
+            # First, try stoichiometric breakdowns
+            for k in buff:
+                if k not in self.recipe_buff:
+                    # This has no recipe, it's probably ORE, but continue
+                    continue
+                required_units = buff[k]
+                recipe_units = self.recipe_buff[k][0]
+                recipe_elements = self.recipe_buff[k][1]
+                # Do we have an exact breakdown?
+                if required_units % recipe_units == 0:
+                    # Yes! Do it
+                    # Remove from buffer
+                    buff.pop(k)
+                    # Add in required elements in quantity
+                    multiples = required_units // recipe_units
+                    for elem in recipe_elements:
+                        # Initialise if not present
+                        if elem not in buff:
+                            buff[elem] = 0
+                        buff[elem] += recipe_elements[elem] * multiples
+                    # Start the loop again
+                    break
+                else:
+                    # No, this is messy, try another for now
+                    continue
+            else:
+                # No stoichiometric options available.
+                # Pick the highest level one
+                # to deal with first.
+                # # print(self.recipe_buff)
+                # # print(buff)
+                # # print(self.element_levels)
+                # Identify the highest level thing remaining and
+                # break that wastefully.
+                scores = [(k, self.element_levels[k]) for k in buff]
+                # # print(scores)
+                # Get the highest
+                highest = scores[0]
+                for elem in scores:
+                    if elem[1] > highest[1]:
+                        highest = elem
+                highest = highest[0]
+                # # print(highest)
+                # Break it
+                required_units = buff[highest]
+                recipe_units = self.recipe_buff[highest][0]
+                recipe_elements = self.recipe_buff[highest][1]
+                multiples = (required_units // recipe_units) + 1
+                # Do it
+                buff.pop(highest)
+                for elem in recipe_elements:
+                    # Initialise if not present
+                    if elem not in buff:
+                        buff[elem] = 0
+                    buff[elem] += recipe_elements[elem] * multiples
+                # Carry on!
+                # raise ValueError("No tidy breakdowns found in iteration. :(")
+
+        # End Result
+        print("Done")
+        print(buff)
+
+
+r = Recipe.from_file('014-recipe-2.txt')
+# print(r.recipe_buff)
+r.solve()
