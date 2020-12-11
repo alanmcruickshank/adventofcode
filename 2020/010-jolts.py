f1db431d0c139dbe6d872c18db3754c8d7e1de19
Alan <alanmcruickshank@gmail.com>
2020-12-11 10:37:56 +0000

    Day 10 part 2

--- 2020/010-jolts.py
+++ 2020/010-jolts.py
@@ -35,6 +35,30 @@ def count_diffs(diff_iter):
         counts[d] += 1
     return counts
 
+def populate_path_counts(devices, start=0, end_diff=3, max_diff=3):
+    path_counts = {}
+    devices = devices.copy()
+    # Add the end point
+    devices.append(max(devices) + end_diff)
+    # Add the start point
+    devices.append(start)
+    # Sort first. This is important.
+    devices = sorted(devices)
+    # Add the start point
+    path_counts[start] = 1
+    # Iterate through devices, adding up the paths
+    for idx, dev in enumerate(devices):
+        # Where can I get to from this point?
+        # We know it will be within the next three points
+        for next_dev in devices[idx + 1 : idx + 1 + max_diff]:
+            if next_dev - dev > max_diff:
+                continue
+            if next_dev in path_counts:
+                path_counts[next_dev] += path_counts[dev]
+            else:
+                path_counts[next_dev] = path_counts[dev]
+    return path_counts
+
 for fname in inputs:
     print(fname)
     devs = load_and_sort(fname)
@@ -43,3 +67,16 @@ for fname in inputs:
     print(counts)
     print(counts[1] * counts[3])
     # Part 1 answer: 1984
+
+    # Part 2 is graph theory.
+    # Dijkstras algorithm? Not quite.
+    # For each adapter, we need to know how many ways we can get TO IT.
+
+    device_rating = max(devs) + 3
+    # We cannot get to a device from another one higher than it.
+    # Therefore work through the adapters, populating
+    # a set of ways you could arrive at this point.
+    # We need to add up the ways as we go.
+    path_counts = populate_path_counts(devs)
+    print(path_counts[device_rating])
+    # Part 2 answer: 3543369523456
