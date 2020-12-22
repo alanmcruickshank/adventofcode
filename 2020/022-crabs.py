85a92905751d24b8af43cf314a90694e900a4bbb
Alan <alanmcruickshank@gmail.com>
2020-12-22 10:41:44 +0000

    Day 22 part 2

--- 2020/022-crabs.py
+++ 2020/022-crabs.py
@@ -10,7 +10,50 @@ def calc_scores(deck):
     return sum((idx + 1) * elem for idx, elem in enumerate(reversed(deck)))
 
 
-for fname in ['022-1.txt', '022-2.txt']:
+def play_regular_combat(p1_deck, p2_deck, recurse=False):
+    p1_deck = p1_deck.copy()
+    p2_deck = p2_deck.copy()
+    previous_game_positions = set()
+    previous_game_positions.add(tuple([tuple(p1_deck), tuple(p2_deck)]))
+    while p1_deck and p2_deck:
+        # Pop off the cards.
+        p1_card, p2_card = p1_deck.pop(0), p2_deck.pop(0)
+        # if recurse AND enough cards, recurse. Otherwise normal.
+        if recurse and len(p1_deck) >= p1_card and len(p2_deck) >= p2_card:
+            # Recursive game!
+            winner, _ = play_regular_combat(p1_deck[:p1_card], p2_deck[:p2_card], recurse=True)
+        else:
+            if p1_card > p2_card:
+                winner = 1
+            elif p2_card > p1_card:
+                winner = 2
+            else:
+                raise ValueError("Huh!?")
+
+        if winner == 1:
+            p1_deck += [p1_card, p2_card]
+        elif winner == 2:
+            p2_deck += [p2_card, p1_card]
+        else:
+            raise ValueError("Wat!?")
+
+        game_pos = tuple([tuple(p1_deck), tuple(p2_deck)])
+        if game_pos in previous_game_positions:
+            # Win in favour of p1
+            p2_deck = []
+            break
+        previous_game_positions.add(game_pos)
+    # Check winner and calculate score.
+    if p1_deck:
+        score = calc_scores(p1_deck)
+        winner = 1
+    elif p2_deck:
+        score = calc_scores(p2_deck)
+        winner = 2
+    return winner, score
+
+
+for fname in ['022-3.txt', '022-1.txt', '022-2.txt']:
     print(fname)
     with open(fname) as f:
         p1, _, p2 = f.read().partition('\n\n')
@@ -18,20 +61,8 @@ for fname in ['022-1.txt', '022-2.txt']:
     p1_deck = [int(elem) for elem in p1.strip().splitlines(keepends=False)[1:]]
     p2_deck = [int(elem) for elem in p2.strip().splitlines(keepends=False)[1:]]
     # Play the game
-    while p1_deck and p2_deck:
-        # Pop off the cards.
-        p1_card, p2_card = p1_deck.pop(0), p2_deck.pop(0)
-        if p1_card > p2_card:
-            p1_deck += [p1_card, p2_card]
-        elif p2_card > p1_card:
-            p2_deck += [p2_card, p1_card]
-        else:
-            ValueError("Huh!?")
-    # Check winner and calculate score.
-    if p1_deck:
-        print("P1 wins.")
-        score = calc_scores(p1_deck)
-    elif p2_deck:
-        print("P2 wins.")
-        score = calc_scores(p2_deck)
-    print("Score:", score)
+    winner, score = play_regular_combat(p1_deck, p2_deck, recurse=False)
+    print("No Recurse Winner", winner, "Score", score)
+    # Part 1 answer: 33393
+    winner, score = play_regular_combat(p1_deck, p2_deck, recurse=True)
+    print("Recurse Winner", winner, "Score", score)
