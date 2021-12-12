3988f389ca57b693d5af15b0c7e68a221686d8f4
Alan <alanmcruickshank@gmail.com>
2021-12-12 19:25:48 +0000

    day 1 part 1 working

--- 2021/001-part1.hs
+++ 2021/001-part1.hs
@@ -2,6 +2,16 @@ import System.IO
 
 main = do
     f <- readFile "001-input.txt"
-    let l = lines f
-    let n = length l
+    -- Get the lines and convert to integer
+    let l = map read (lines f)::[Integer]
+    -- Zip up the lines with itself (minus one) to compare pairs
+    let p = zip l (drop 1 l)
+    let n = length p
     print n
+    print (head p)
+    let c = map pair_greater p
+    print (head c)
+    let r = sum c
+    print r
+
+pair_greater (a, b) = (if a < b then 1 else 0)
