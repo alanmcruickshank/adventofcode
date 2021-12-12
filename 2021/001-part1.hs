4b074e6940986e24988ae98a315556061dd26036
Alan <alanmcruickshank@gmail.com>
2021-12-12 19:27:36 +0000

    tidy up part 1

--- 2021/001-part1.hs
+++ 2021/001-part1.hs
@@ -6,12 +6,9 @@ main = do
     let l = map read (lines f)::[Integer]
     -- Zip up the lines with itself (minus one) to compare pairs
     let p = zip l (drop 1 l)
-    let n = length p
-    print n
-    print (head p)
+    -- Compare pairs and get a list of integers
     let c = map pair_greater p
-    print (head c)
-    let r = sum c
-    print r
+    -- Sum the integers
+    print (sum c)
 
 pair_greater (a, b) = (if a < b then 1 else 0)
