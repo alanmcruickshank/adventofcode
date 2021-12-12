751af94da90a6b351d3df1fbb97886d694efec93
Alan <alanmcruickshank@gmail.com>
2021-12-12 18:14:39 +0000

    working way to do something with a file

--- 2021/001-part1.hs
+++ 2021/001-part1.hs
@@ -1,7 +1,7 @@
 import System.IO
 
 main = do
-    h <- openFile "001-input.txt" ReadMode
-    var <- hGetLine h
-    putStrLn var
-    hClose h
+    f <- readFile "001-input.txt"
+    let l = lines f
+    let n = length l
+    print n
