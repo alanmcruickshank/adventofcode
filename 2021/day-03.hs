6f921d12b10721e5e829cd39689da92cec6abf74
Alan <alanmcruickshank@gmail.com>
2021-12-13 21:43:18 +0000

    Tidy up day 3 a bit more

--- 2021/day-03.hs
+++ 2021/day-03.hs
@@ -13,15 +13,6 @@ main = do
 
 -- ------------ PART 1 Functions
 
--- Convert single char to a pair
-convert_char        :: Char -> (Integer, Integer)
-convert_char '0'    = (1, 0)
-convert_char '1'    = (0, 1)
-
--- Convert pairs back to integers
-convert_pair        :: (Integer, Integer) -> Integer
-convert_pair (a, b) = if a > b then 0 else 1
-
 -- Convert list of bits to integer.
 -- NB: Using a lambda expression for folded bitshifting
 bitlist_to_int          :: [Integer] -> Integer
@@ -32,26 +23,31 @@ bitlist_complement l    = map (\x -> 1 - x) l
 
 -- Convert a string to a list of pairs
 convert_string      :: String -> [(Integer, Integer)]
-convert_string s    = map convert_char s
-
--- Add two pairs
-sum_pair                :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
-sum_pair (a, b) (c, d)  = (a + c, b + d)
+convert_string s    =
+    let c '0'       = (1, 0)
+        c '1'       = (0, 1)
+    in map c s
 
 -- Add two pair strings.
 -- The uncurry takes sum_pair (which expects two inputs), to something which affects one as a pair as returned from zip.
-sum_lists               :: [(Integer, Integer)] -> [(Integer, Integer)] -> [(Integer, Integer)]
-sum_lists a b           = map (uncurry sum_pair) (zip a b)
+sum_lists                           :: [(Integer, Integer)] -> [(Integer, Integer)] -> [(Integer, Integer)]
+sum_lists a b                       = map (uncurry sum_pair) (zip a b)
+    where sum_pair (a, b) (c, d)    = (a + c, b + d)
 
 -- Go straight from lines to counts
-lines_to_counts l       = foldl1 sum_lists (map convert_string l)
+lines_to_counts                     :: [String] -> [(Integer, Integer)]
+lines_to_counts l                   = foldl1 sum_lists (map convert_string l)
 
 -- Go staight from counts to combined factor
-counts_to_bitlist c     = map convert_pair c
-bitlist_to_factor b     = (bitlist_to_int b) * ((bitlist_to_int.bitlist_complement) b)
-counts_to_basic_factor  = bitlist_to_factor.counts_to_bitlist
+counts_to_bitlist                   :: [(Integer, Integer)] -> [Integer]
+counts_to_bitlist c                 =
+    let convert_pair (a, b)         = if a > b then 0 else 1
+    in map convert_pair c
 
-lines_to_factors1       = counts_to_basic_factor.lines_to_counts
+lines_to_factors1                   :: [String] -> Integer
+lines_to_factors1                   = counts_to_factors1.lines_to_counts
+    where counts_to_factors1        = bitlist_to_factors1.counts_to_bitlist
+          bitlist_to_factors1 b     = (bitlist_to_int b) * ((bitlist_to_int.bitlist_complement) b)
 
 -- ------------ PART 1 Functions
 
@@ -68,7 +64,9 @@ desired_char_at_index_from_lines       :: ([String], Int) -> Bool -> Char
 desired_char_at_index_from_lines a b   = desired_char_from_pair (get_pair_at_index_from_lines a) b
 
 filter_lines_a                :: ([String], Int) -> Bool -> ([String], Int)
-filter_lines_a (l, i) b       = (filter (\x -> x !! i == (desired_char_at_index_from_lines (l, i) b)) l, i + 1)
+filter_lines_a (l, i) b       = (filter filter_func l, i + 1)
+    where filter_func x       = x !! i == char_at_idx
+          char_at_idx         = desired_char_at_index_from_lines (l, i) b
 
 filter_lines_b                :: ([String], Int) -> Bool -> String
 filter_lines_b ((x:[]), _) _  = x          -- Single value left
@@ -77,10 +75,7 @@ filter_lines_b ((x:xs), i) b  = filter_lines_b (filter_lines_a ((x:xs), i) b) b 
 filter_lines                  :: [String] -> Bool -> String
 filter_lines l b              = filter_lines_b (l, 0) b
 
-string_to_bitlist             :: [Char] -> [Integer]
-string_to_bitlist l           = map (\x -> if x == '0' then 0 else 1) l
-
-binstring_to_int              :: String -> Integer
-binstring_to_int              = bitlist_to_int.string_to_bitlist
-
+lines_to_factors2             :: [String] -> Integer
 lines_to_factors2 l           = (binstring_to_int(filter_lines l True)) * (binstring_to_int(filter_lines l False))
+    where binstring_to_int    = bitlist_to_int.string_to_bitlist
+          string_to_bitlist l = map (\x -> if x == '0' then 0 else 1) l
