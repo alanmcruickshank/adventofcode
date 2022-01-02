4f0e86018ee7d7b805bf55f94e2abdf763baf556
Alan <alanmcruickshank@gmail.com>
2022-01-02 20:18:02 +0000

    part way to part 2

--- 2021/day-09.hs
+++ 2021/day-09.hs
@@ -3,16 +3,23 @@
 import Data.Maybe (fromJust, isNothing, isJust)
 import Data.List (elemIndex, find, (\\), intersect, sort, transpose)
 import Data.Char (digitToInt)
+import qualified Data.Map as Map 
 
 main = do
-    f <- readFile "day-09-input.txt"
+    f <- readFile "day-09-example.txt"
     let i = parseInput f
     putStrLn "=== Part 1"
     print (answer1 i)
+    putStrLn "=== Part 2"
+    print (answer2 i)
 
 -- ----- File Processing
 
-type GridMap              = ([[Int]], (Int, Int))
+type GridPoint              = (Int, Int)                -- x/y 
+type GridMap                = ([[Int]], (Int, Int))     -- width/height
+type GridSquare             = ([[Int]], GridPoint)
+type GridPointScore         = (GridSquare, Int, Int, Maybe GridPoint)
+
 
 parseInput      :: String -> GridMap
 parseInput s    = (l', (w, h))
@@ -34,15 +41,16 @@ windows w (x:xs)
     | length (x:xs) == w    = [(x:xs)]
     | otherwise             = (take w (x:xs)):(windows w xs)
 
-gridWindows                 :: Int -> GridMap -> [GridMap]
-gridWindows n (m, (w, h))   = map toGrid squares
-    where row_groups        = windows n m
+gridWindows                 :: GridMap -> [GridSquare]
+gridWindows unpadded        = indexed_squares
+    where (m, (w, h))       = pad unpadded
+          row_groups        = windows 3 m
           row_group_slices  = map (transpose.(map (windows 3))) row_groups
           squares           = foldl1 (++) row_group_slices
-          toGrid s          = (s, (n, n))
+          indexed_squares   = zip squares (map (\i -> (mod i (w - 2), div i (w - 2))) [0..])
 
 -- Assume map of size 3
-riskLevel                   :: GridMap -> Int
+riskLevel                   :: GridSquare -> Int
 riskLevel (m, _)            = if is_low then c + 1 else 0
     where c                 = (m !! 1) !! 1
           u                 = (m !! 0) !! 1
@@ -55,4 +63,19 @@ riskLevel (m, _)            = if is_low then c + 1 else 0
 -- ----- File Processing
 
 answer1                     :: GridMap -> Int
-answer1 i                   = sum (map riskLevel (gridWindows 3 (pad i)))
+answer1 i                   = sum (map riskLevel (gridWindows i))
+
+answer2                     :: GridMap -> Map.Map GridPoint GridPointScore
+answer2 i                   = stepBasin (basinBasis i)
+
+basinBasis                  :: GridMap -> Map.Map GridPoint GridPointScore
+basinBasis i                = m
+    where windows           = gridWindows i
+          m                 = Map.fromList (map f windows)
+          f (m, p)          = (p, ((m, p), c, r, if r > 0 then Just p else Nothing))
+            where r         = riskLevel (m, p)
+                  c         = (m !! 1) !! 1
+
+stepBasin                   :: Map.Map GridPoint GridPointScore ->  Map.Map GridPoint GridPointScore
+stepBasin m                 = assigned -- m'
+    where assigned          = Map.filter (\(_, _, _, p) -> isJust p) m
