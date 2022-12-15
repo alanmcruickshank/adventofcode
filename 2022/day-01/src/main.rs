73ec105f6102e3f50ce76b6e3c10b68864dffbd8
Alan Cruickshank <alan@designingoverload.com>
2022-12-15 17:12:19 +0000

    day 1 part 2

--- 2022/day-01/src/main.rs
+++ 2022/day-01/src/main.rs
@@ -9,19 +9,23 @@ use std::path::Path;
 fn main() {
     println!("Hello, world!");
 
-    let mut highest_total = 0;
+    let mut highest_totals = [0, 0, 0];
 
     // File input must exist in current path before this produces output
     if let Ok(lines) = read_lines("input.txt") {
-        // Consumes the iterator, returns an (Optional) String
         let mut calorie_sum = 0;
         for line in lines {
             if let Ok(line_text) = line {
                 if line_text == "" {
-                    if calorie_sum > highest_total {
-                        highest_total = calorie_sum;
+                    if calorie_sum > highest_totals[2] {
+                        highest_totals.rotate_left(1);
+                        highest_totals[2] = calorie_sum;
+                    } else if calorie_sum > highest_totals[1] {
+                        highest_totals[0] = highest_totals[1];
+                        highest_totals[1] = calorie_sum;
+                    } else if calorie_sum > highest_totals[0] {
+                        highest_totals[0] = calorie_sum;
                     }
-                    // println!("Calorie Sum: {}", calorie_sum);
                     calorie_sum = 0;
                 } else {
                     let parsed: i32 = line_text.parse().unwrap();
@@ -29,10 +33,11 @@ fn main() {
                 }
             }
         }
-        // println!("Final Calorie Sum: {}", calorie_sum);
     }
 
-    println!("Final highest sum: {}", highest_total);
+    println!("Final highest sums: {:?}", highest_totals);
+    let sum: i32 = highest_totals.iter().sum();
+    println!("Final highest sum totals: {}", sum);
 
 }
 
